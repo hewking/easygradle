db25a031b8887162d5c300e172d06f47003fd30e
hewking <cjh1095358798@gmail.com>
2020-08-06 08:16:55 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # easygradle
 easygradle
+
+#### 参考资料
+- [gradle docs 6.5.1](https://docs.gradle.org/current/userguide/custom_plugins.html#sec:custom_plugins_standalone_project)
--- my-gradle-plugin/src/main/groovy/com/hewking/plugin/MyPlugin2.groovy
+++ my-gradle-plugin/src/main/groovy/com/hewking/plugin/MyPlugin2.groovy
@@ -1,7 +1,7 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class MyPlugin implements Plugin<Project>{
+class MyPlugin2 implements Plugin<Project>{
 
     @Override
     void apply(Project project) {
